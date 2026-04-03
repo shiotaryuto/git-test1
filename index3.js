142a5c81b5905e20060af828a4e64184db26ec8a
shiota <sryu0116@gmail.com>
2026-04-03 14:51:38 +0900

    挨拶関数を実装した

--- index3.js
+++ index3.js
@@ -1,2 +1,8 @@
+<<<<<<< HEAD
 function greet(){
   console.log("Hello from the remote branch")
+=======
+function greet() {
+    console.log("Hello from the local branch!");
+}
+>>>>>>> a32e3b1 (挨拶関数を実装した)
